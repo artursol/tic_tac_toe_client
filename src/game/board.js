4d26d7061c3734a68db15bb675431d48c84a80f5
Arturas Solncevas <as@centic.dk>
2021-04-24 18:55:11 +0200

    Add board and field logics

--- src/game/board.js
+++ src/game/board.js
@@ -1,3 +1,6 @@
+import $ from 'jquery';
+import Field from './field';
+
 class Board {
   constructor(el, gameContext, options) {
     const { size, fieldOptions } = options;
@@ -5,6 +8,31 @@ class Board {
     this.mountElement = el;
     this.gameContext = gameContext;
     this.fieldOptions = fieldOptions;
+
+    this.setup();
+  }
+
+  setup() {
+    this.fields = [];
+    this.createFields();
+  }
+
+  createFields() {
+    for (let row = 0; row < this.size; row++) {
+      const rowEl = $("<div class='row'></div>");
+      this.mountElement.append(rowEl);
+
+      for (let col = 0; col < this.size; col++) {
+        const field = new Field([row, col], this.gameContext, this.fieldOptions);
+        rowEl.append(field.domElement);
+        this.fields.push(field);
+      }
+    }
+  }
+
+  reset() {
+    this.mountElement.empty();
+    this.setup();
   }
 }
 
--- src/game/field.js
+++ src/game/field.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import '../css/field.scss';
 
 class Field {
   constructor(position, gameContext, options) {
@@ -11,6 +12,36 @@ class Field {
     this.gameContext = gameContext;
     this.onClickCallback = onClick;
     this.domElement = $('<div class=\'field\'></div>');
+
+    this.setDiagonals();
+    this.setEvents();
+  }
+
+  setEvents() {
+    this.domElement.click(async () => {
+      if (this.touched || this.gameContext.status !== 1) return;
+
+      const { numberOfMoves, actionType, gameId } = this.gameContext;
+      await this.onClickCallback(numberOfMoves + 1, actionType, gameId, this.position);
+      this.touched = true;
+      this.type = this.gameContext.actionType;
+      this.domElement.addClass(`touched ${this.type}`);
+      this.gameContext.update({
+        type: this.type,
+        position: this.position,
+        diagonals: [
+          this.inDiagonal1,
+          this.inDiagonal2,
+        ],
+      });
+    });
+  }
+
+  setDiagonals() {
+    const { diagonal1, diagonal2 } = this.gameContext.diagonalPositions;
+    [diagonal1, diagonal2].forEach((positions, index) => {
+      this[`inDiagonal${index + 1}`] = !!positions.find((e) => e[0] === this.position[0] && e[1] === this.position[1]);
+    });
   }
 }
 
